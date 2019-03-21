ac3062e70918f021e7a566b47c267ce826c17769
mateBalint <matebalint98@gmail.com>
2019-03-21 14:46:28 +0100

    Filtering by product category and supplier

--- src/main/java/com/codecool/shop/config/Initializer.java
+++ src/main/java/com/codecool/shop/config/Initializer.java
@@ -20,24 +20,42 @@ public class Initializer implements ServletContextListener {
         SupplierDao supplierDataStore = SupplierDaoMem.getInstance();
 
         //setting up a new supplier
-        Supplier amazon = new Supplier("Amazon", "Digital content and services");
-        supplierDataStore.add(amazon);
-        Supplier lenovo = new Supplier("Lenovo", "Computers");
-        supplierDataStore.add(lenovo);
+        Supplier bloomsbury = new Supplier("Bloomsbury Publishing PLC", "Book service");
+        supplierDataStore.add(bloomsbury);
+
+        Supplier harperCollins = new Supplier("HarperCollins Publishers", "Book service");
+        supplierDataStore.add(harperCollins);
+
+        Supplier walker = new Supplier("Walker Books Ltd", "Book service");
+        supplierDataStore.add(walker);
+
+        Supplier penguin = new Supplier("Penguin Putnam Inc", "Book service");
+        supplierDataStore.add(penguin);
 
         //setting up a new product category
-        ProductCategory tablet = new ProductCategory("Best Sellers", "Book", "A tablet computer, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display.");
-        productCategoryDataStore.add(tablet);
+        ProductCategory bestSeller = new ProductCategory("Bestseller", "Bestseller", "This is a book from the bestseller section.");
+        productCategoryDataStore.add(bestSeller);
+
+        ProductCategory gastronomy = new ProductCategory("Gastronomy", "Gastronomy", "This is a book from the gastronomy section.");
+        productCategoryDataStore.add(gastronomy);
+
+        ProductCategory scifi = new ProductCategory("SciFi", "SciFi", "This is a book from the SciFi section.");
+        productCategoryDataStore.add(scifi);
+
+        ProductCategory horror = new ProductCategory("Horror", "Horror", "This is a book from the horror section.");
+        productCategoryDataStore.add(horror);
+
+        ProductCategory history = new ProductCategory("History", "History", "This is a book from the history section.");
+        productCategoryDataStore.add(history);
+
 
         //setting up products and printing it
-        productDataStore.add(new Product("Wuthering Heights", 7.62f, "USD", "Emily Bronte's only novel, a work of tremendous and far-reaching influence.", tablet, amazon));
-        productDataStore.add(new Product("Thunderhead", 10.37f, "USD", "The dark and thrilling sequel to Scythe, the New York Times sci-fi bestseller.", tablet, lenovo));
-        productDataStore.add(new Product("All Quiet on the Western Front", 10.02f, "USD", "One by one the boys begin to fall...", tablet, amazon));
-        productDataStore.add(new Product("Sky in the Deep", 17.89f, "USD", "A 2018 Most Anticipated Young Adult book from debut author Adrienne Young.", tablet, amazon));
-        productDataStore.add(new Product("Journey to the End of the Night", 18.39f, "USD", "Journey to the End of the Night is a literary symphony of violence, cruelty and obscene nihilism.", tablet, amazon));
-        productDataStore.add(new Product("Together: Our Community Cookbook", 17.15f, "USD", "Together celebrates the power of cooking to connect us to one another.", tablet, amazon));
-        productDataStore.add(new Product("Les Miserables", 16.24f, "USD", "Victor Hugo's tale of injustice, heroism and love.", tablet, amazon));
-        productDataStore.add(new Product("Tales from the Inner City", 26.62f, "USD", "Where can we live if not in each other's shadow?", tablet, amazon));
-
+        productDataStore.add(new Product("Wuthering Heights", 7.62f, "USD", "Emily Bronte's only novel, a work of tremendous and far-reaching influence.", bestSeller, penguin));
+        productDataStore.add(new Product("Thunderhead", 10.37f, "USD", "The dark and thrilling sequel to Scythe, the New York Times sci-fi bestseller.", horror, bloomsbury));
+        productDataStore.add(new Product("Sky in the Deep", 17.89f, "USD", "A 2018 Most Anticipated Young Adult book from debut author Adrienne Young.", gastronomy, harperCollins));
+        productDataStore.add(new Product("Journey to the End of the Night", 18.39f, "USD", "Journey to the End of the Night is a literary symphony of violence, cruelty and obscene nihilism.", bestSeller, walker));
+        productDataStore.add(new Product("Together: Our Community Cookbook", 17.15f, "USD", "Together celebrates the power of cooking to connect us to one another.", scifi, penguin));
+        productDataStore.add(new Product("Les Miserables", 16.24f, "USD", "Victor Hugo's tale of injustice, heroism and love.", horror, bloomsbury));
+        productDataStore.add(new Product("Tales from the Inner City", 26.62f, "USD", "Where can we live if not in each other's shadow?", history, penguin));
     }
 }
--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -2,9 +2,11 @@ package com.codecool.shop.controller;
 
 import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.SupplierDao;
 import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
 import com.codecool.shop.dao.implementation.ProductDaoMem;
 import com.codecool.shop.config.TemplateEngineUtil;
+import com.codecool.shop.dao.implementation.SupplierDaoMem;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
@@ -23,7 +25,10 @@ public class ProductController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         ProductDao productDataStore = ProductDaoMem.getInstance();
+
         ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
+
+        SupplierDao supplierDataStore = SupplierDaoMem.getInstance();
 
 //        Map params = new HashMap<>();
 //        params.put("category", productCategoryDataStore.find(1));
@@ -31,11 +36,48 @@ public class ProductController extends HttpServlet {
 
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
-//        context.setVariables(params);
-        context.setVariable("recipient", "World");
-        context.setVariable("category", productCategoryDataStore.find(1));
-        context.setVariable("products", productDataStore.getBy(productCategoryDataStore.find(1)));
-        engine.process("product/index.html", context, resp.getWriter());
-    }
+//          context.setVariables(params);
+            context.setVariable("recipient", "World");
+            context.setVariable("categories", productCategoryDataStore.getAll());
+            context.setVariable("products", productDataStore.getAll());
+            context.setVariable("publishers", supplierDataStore.getAll());
 
+            if (!req.getParameterNames().hasMoreElements() || req.getParameterValues(req.getParameterNames().nextElement())[0].equals("All")) {
+                context.setVariable("products", productDataStore.getAll());
+            } else if (req.getParameterNames().nextElement().equals("categories")) {
+
+                if (req.getParameter("categories").equals("Bestseller")) {
+                    context.setVariable("products", productDataStore.getBy(productCategoryDataStore.find(1)));
+
+                } else if (req.getParameter("categories").equals("Gastronomy")) {
+                    context.setVariable("products", productDataStore.getBy(productCategoryDataStore.find(2)));
+
+                } else if (req.getParameter("categories").equals("SciFi")) {
+                    context.setVariable("products", productDataStore.getBy(productCategoryDataStore.find(3)));
+
+                } else if (req.getParameter("categories").equals("Horror")) {
+                    context.setVariable("products", productDataStore.getBy(productCategoryDataStore.find(4)));
+
+                } else if (req.getParameter("categories").equals("History")) {
+                    context.setVariable("products", productDataStore.getBy(productCategoryDataStore.find(5)));
+                }
+
+
+            } else if (req.getParameterNames().nextElement().equals("publishers")) {
+
+                    if (req.getParameter("publishers").equals("Bloomsbury Publishing PLC")) {
+                        context.setVariable("products", productDataStore.getBy(supplierDataStore.find(1)));
+
+                    } else if (req.getParameter("publishers").equals("HarperCollins Publishers")) {
+                        context.setVariable("products", productDataStore.getBy(supplierDataStore.find(2)));
+
+                    } else if (req.getParameter("publishers").equals("Walker Books Ltd")) {
+                        context.setVariable("products", productDataStore.getBy(supplierDataStore.find(3)));
+
+                    } else if (req.getParameter("publishers").equals("Penguin Putnam Inc")) {
+                        context.setVariable("products", productDataStore.getBy(supplierDataStore.find(4)));
+                    }
+            }
+            engine.process("product/index.html", context, resp.getWriter());
+    }
 }
